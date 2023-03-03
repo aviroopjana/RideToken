abf257a51c45d6c8cc819d3931a660f8bc881a78
Aviroop Jana <aviroopjana@gmail.com>
2023-03-03 23:59:34 +0530

    updated contract

--- contracts/RideToken.sol
+++ contracts/RideToken.sol
@@ -30,38 +30,6 @@ contract RideToken {
         renters[walletAddress] = Renter(walletAddress, firstname, lastname, canRent, active, balance, due, start, end);
     }
 
-    //SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-contract RideToken {
-
-    address owner;
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    //Add Yourself as a Renter
-
-    struct Renter {
-        address payable walletAddress;
-        string firstname;
-        string lastname;
-        bool canRent;
-        bool active;
-        uint balance;
-        uint due;
-        uint start;
-        uint end;
-    }
-
-    mapping (address => Renter) public renters;
-
-    function addRenter(address payable walletAddress, string memory firstname, string memory lastname, bool canRent, bool active, uint balance, uint due, uint start, uint end) public {
-        renters[walletAddress] = Renter(walletAddress, firstname, lastname, canRent, active, balance, due, start, end);
-    }
-
     //CheckOut Bike
 
     function CheckOut(address walletAddress) public {
@@ -146,6 +114,9 @@ contract RideToken {
         canRent = renters[walletAddress].canRent;
         active = renters[walletAddress].active;
     }
+
+    // function renterExists(address walletAddress) public view returns (bool) {
+    //     if (renters[walletAdress].walletAddress)
+    // }
 }
 
-}
